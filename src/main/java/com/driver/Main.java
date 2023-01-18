1dd014e367873a4ad3e70748533279e164a9b89f
yogita <yogitajangir04@gmail.com>
2023-01-18 09:02:35 +0530

    next

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,24 +3,24 @@ package com.driver;
 public class Main {
     public static void main(String[] args) {
         Product p = new Product();
-       int x =  p.product(2,3);
-       int y =  p.product(3,4,5);
-       double z =  p.product(2.3,2.3);
-    }
-}
-class Product{
-
-    public  int product(int x, int y)
-    {
-     return x+y;
-    }
-    public int product(int x, int y, int z)
-    {
-       return x+y+z;
-    }
-    public double product(double x , double y)
-    {
-        return x+y;
+        int x = p.product(2, 3);
+        int y = p.product(3, 4, 5);
+        double z = p.product(2.3, 2.3);
     }
 
+    public static  class Product {
+
+        public int product(int x, int y) {
+            return x + y;
+        }
+
+        public int product(int x, int y, int z) {
+            return x + y + z;
+        }
+
+        public double product(double x, double y) {
+            return x + y;
+        }
+
+    }
 }
